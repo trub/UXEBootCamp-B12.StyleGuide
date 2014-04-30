c89018495c6279f32aae246d7d62344a596e9462
matthew weintrub <heytrub@gmail.com>
2014-04-30 12:45:33 -0700

    change code blocks html presentation to jade

---
+++ public/javascript/main.js
@@ -0,0 +1,32 @@
+$(function() {
+	var $menu = $('nav#menu'),
+		$html = $('html, body');
+
+	$menu.mmenu();
+	$menu.find( 'li > a' ).on(
+		'click',
+		function()
+		{
+			var href = $(this).attr( 'href' );
+
+			//	if the clicked link is linked to an anchor, scroll the page to that anchor 
+			if ( href.slice( 0, 1 ) == '#' )
+			{
+				$menu.one(
+					'closed.mm',
+					function()
+					{
+						setTimeout(
+							function()
+							{
+								$html.animate({
+									scrollTop: $( href ).offset().top
+								});	
+							}, 10
+						);	
+					}
+				);						
+			}
+		}
+	);
+});
