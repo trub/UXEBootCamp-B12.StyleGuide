e37d4401b949f93ac7582e641a9a470a52a998ce
nattyc <natty.coleman@gmail.com>
2014-05-01 15:35:53 -0700

    adds code view toggle

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,18 @@
 $(".off-canvas-nav").on("click",function(){
 	$("nav").toggleClass("mobile-nav");
 	$("div#sg-wrapper").toggleClass("sg-canvas");
-})
+});
+
+$("#code-toggle-typography").on("click",function(){
+  $(".code-block-typography").toggleClass("code-show");
+});
+
+$("#code-toggle-icons").on("click",function(){
+  $(".code-block-icons").toggleClass("code-show");
+});
+
+$("#code-toggle-buttons").on("click",function(){
+  $(".code-block-buttons").toggleClass("code-show");
+});
+
+
